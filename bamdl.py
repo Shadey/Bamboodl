07023c5d0d0e224356e9b38c9a52d183cd41a29b
shadey <me@shadey.net>
2015-05-20 23:42:02 -0400

    Console version rewrite

--- bamdl.py
+++ bamdl.py
@@ -1,22 +1,23 @@
-import time,os,json,socket
+import time,os,json,socket,concurrent.futures,sys
+from urllib.error import *
 from urllib.parse import urlparse
 from urllib.request import Request
 from urllib.request import urlopen
-def chandl(link,host):
-    base = ""
+def chandl(link,host,base=None):
     special = ["b","sp","v","pol"]
     board = urlparse(link).path.split("/")[1]
     link = link.split("#")[0].replace("html","json").strip()
     if "json" not in link: link += ".json"
-    if host == "4chan.org":
-        base = "http://i4cdn.org/{}/".format(board)
+    if host == "boards.4chan.org":
+        base = "http://i.4cdn.org/{}/".format(board)
     elif board in special:
         base = "http://`8ch.net/{}/src/".format(board)
+    elif host == "lainchan.org":
+        base = "http://lainchan.org/{}/src/".format(board)
     else:
         base = "https://media.8ch.net/{}/src/".format(board)
     try:
         jsondata = json.loads(urlopen(Request(link, headers={'User-Agent': "Mozilla 5.0"}),timeout=30).read().decode("UTF-8"))
-        print(jsondata["posts"])
         for post in jsondata["posts"]:
             if "tim" in post:
                 filename = "{}{}".format(post["tim"],post["ext"])
@@ -35,27 +36,30 @@ def chandl(link,host):
     except(ValueError,KeyError,TypeError) as err:
         print("Parsing Error:",err)
     except socket.timeout:
-        print("Json timed out")
-    except Exception as et:
-        print("Something strange happened ",et)
-        
-    
+        print("Thread timed out")
+    except (HTTPError,URLError) as err:
+        print("Thread 404ed")
+        deadlinks.append(link)
 def watch(f):
+    starttime = time.time()
     f.seek(0,2)
     while True:
         lines = f.readlines()
         if not lines:
-            time.sleep(0.1)
-            continue
-        for line in lines: yield line
+            if int((time.time()- starttime)) >= 500:
+                starttime = time.time()
+                time.sleep(0.1)
+                main()
+        yield [line for line in lines if check(line)]
 def check(url):
     parsed = urlparse(url)
     if (parsed.scheme == "https" or parsed.scheme == "http") and parsed.netloc in hosts and parsed.path != "":
-        return True
+        if not url in deadlinks:return True
+    print("Not valid",url)
     return False
 def dlfile(src,filename):
     try:
-        if os.path.isfile(filename): return 
+        if os.path.isfile(filename): return
         with urlopen(Request(src,headers={"User-Agent": "Mozilla 5.0"}),timeout=300) as resp ,open(filename,"wb") as f:
             print("Downloading",src)
             f.write(resp.read())
@@ -64,20 +68,33 @@ def dlfile(src,filename):
         print("File Error:",error)
     except socket.timeout:
         print("File",src,"timed out")
-    except Exception as err:
-        print("Something strange some happened",error)
         
-def download(link):
-    thread = urlparse(link)
-    for host,func in hosts.items():
-        if host == thread.netloc:
-            func(link,host)
+def download(lines):
+    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as pool:
+        for line in lines:
+            for host,func in hosts.items():
+                if host == urlparse(line).netloc:
+                    print("Starting on",line)
+                    pool.submit(func,line,host)
 def main():
     for host in hosts: os.makedirs(host,exist_ok=True)
-    f = open("chicken.txt")
-    for line in watch(f):
-        print(line)
-        download(line) if check(line) else print("Not valid")
-
-hosts ={"4chan.org":chandl,"8ch.net":chandl} 
-main()
+    f = open("new.txt","w+")
+    download([line for line in f.readlines() if check(line)])
+    for lines in watch(f):
+        download(lines)
+    
+if __name__ == '__main__':
+    hosts ={"boards.4chan.org":chandl,
+            "8ch.net":chandl,
+            "lainchan.org":chandl
+    }
+    deadlinks = []
+    try:
+        main()
+    except KeyboardInterrupt:
+        f  = open("new.txt")
+        newlines = [line for line in f.readlines() if check(line)]
+        os.remove("new.txt")
+        new = open("new.txt","w+")
+        for line in newlines:print(line,file=new)
+        print("Closing")
